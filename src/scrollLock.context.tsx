858ead0875d8bf54fa55560e4b7a38c03f0b2298
Yoo Jin Young <yoojinyoung@kakao.com>
2024-05-13 22:11:15 +0900

    feat: base code

---
+++ src/scrollLock.context.tsx
@@ -0,0 +1,81 @@
+import {
+  PropsWithChildren,
+  createContext,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+
+type ScrollLockContext = {
+  lock: (id?: string) => void;
+  release: (id: string) => void;
+  isScrollLocked: boolean;
+};
+
+const initialContextValue: ScrollLockContext = {
+  lock: () => {},
+  release: () => {},
+  isScrollLocked: false,
+};
+
+export const ScrollLockContext = createContext(initialContextValue);
+
+export function ScrollLockProvider({ children }: PropsWithChildren<{}>) {
+  const [scrollLockerIds, setScrollLockerIds] = useState<string[]>([]);
+
+  const lock = () => {
+    const scrollY = window.scrollY;
+    document.body.style.position = "fixed";
+    document.body.style.width = "100%";
+    document.body.style.top = `-${scrollY}px`;
+  };
+
+  const release = () => {
+    const scrollY = document.body.style.top;
+    document.body.style.position = "";
+    document.body.style.top = "";
+    document.body.style.width = "";
+    window.scrollTo({
+      behavior: "instant",
+      left: 0,
+      top: parseInt(scrollY || "0") * -1,
+    });
+  };
+
+  const isScrollLocked = useMemo(
+    () => scrollLockerIds.length > 0,
+    [scrollLockerIds]
+  );
+
+  useEffect(() => {
+    if (isScrollLocked) {
+      lock();
+    } else {
+      release();
+    }
+  }, [isScrollLocked]);
+
+  const value = useMemo(
+    () => ({
+      lock: (id?: string) => {
+        id = id || nanoid();
+        setScrollLockerIds((prev) => [...prev, id as string]);
+
+        return id;
+      },
+      release: (id: string) => {
+        setScrollLockerIds((prev) =>
+          prev.filter((scrollLocker) => scrollLocker !== id)
+        );
+      },
+      isScrollLocked,
+    }),
+    [isScrollLocked]
+  );
+
+  return (
+    <ScrollLockContext.Provider value={value}>
+      {children}
+    </ScrollLockContext.Provider>
+  );
+}
---
+++ src/useScrollLock.tsx
@@ -0,0 +1,3 @@
+function useScrollLock() {}
+
+export default useScrollLock;
